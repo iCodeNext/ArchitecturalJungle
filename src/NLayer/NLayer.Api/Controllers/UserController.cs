caf5e6b4e7c673e32b2025625af707cfae54854b
mohammadKarimi <mha.karimi@gmail.com>
2024-03-23 14:27:08 +0330

    Use Primary Constructor

--- src/NLayer/NLayer.Api/Controllers/UserController.cs
+++ src/NLayer/NLayer.Api/Controllers/UserController.cs
@@ -3,14 +3,9 @@ using NLayer.Domain;
 using NLayer.Services;
 
 namespace NLayer.Controllers;
-public class UserController : Controller
+public class UserController(UserService userService) : Controller
 {
-    private readonly UserService _userService;
-
-    public UserController(UserService userService)
-    {
-        _userService = userService;
-    }
+    private readonly UserService _userService = userService;
 
     public IActionResult Insert(User model)
     {
