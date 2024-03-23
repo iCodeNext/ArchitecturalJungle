50d0c498d1994b976f84993914bc34b3c1d38d84
mohammadKarimi <mha.karimi@gmail.com>
2024-03-23 14:23:02 +0330

    NoArchitecture Project - Init

--- src/NoArchitecture/Controllers/UserController.cs
+++ src/NoArchitecture/Controllers/UserController.cs
@@ -3,14 +3,9 @@ using NoArchitecture.Domain;
 using NoArchitecture.Services;
 
 namespace NoArchitecture.Controllers;
-public class UserController : Controller
+public class UserController(UserService userService) : Controller
 {
-    private readonly UserService _userService;
-
-    public UserController(UserService userService)
-    {
-        _userService = userService;
-    }
+    private readonly UserService _userService = userService;
 
     public IActionResult Insert(User model)
     {
--- src/NoArchitecture/Program.cs
+++ src/NoArchitecture/Program.cs
@@ -1,25 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+var app = builder.Build();
+{
+    app.UseHttpsRedirection();
+    app.UseAuthorization();
+    app.MapControllers();
+    app.Run();
+}
