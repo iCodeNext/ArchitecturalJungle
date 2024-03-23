72a52ab4a258716111353da0b51b4b384ff114a7
mohammadKarimi <mha.karimi@gmail.com>
2024-03-23 14:32:00 +0330

    Refactor Projects

--- src/Hexagonal/Hexagonal.Application/Context/ISmsProvider.cs
+++ src/Hexagonal/Hexagonal.Application/Context/ISmsProvider.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Hexagonal.Application.Context;
+﻿namespace Hexagonal.Application.Context;
 public interface ISmsProvider
 {
     void Send(string message);
--- src/Hexagonal/Hexagonal.Application/Services/UserService.cs
+++ src/Hexagonal/Hexagonal.Application/Services/UserService.cs
@@ -3,27 +3,15 @@ using Hexagonal.Application.Context;
 
 namespace Hexagonal.Application.Services;
 
-public class UserService
+public class UserService(IApplicationDbContext dbContext, ISmsProvider smsProvider)
 {
-    private readonly IApplicationDbContext _dbContext;
-    private readonly ISmsProvider _smsProvider;
-
-  
-
-    public UserService(IApplicationDbContext dbContext, ISmsProvider smsProvider)
-    {
-        _dbContext = dbContext;
-        _smsProvider = smsProvider;
-    }
+    private readonly IApplicationDbContext _dbContext = dbContext;
+    private readonly ISmsProvider _smsProvider = smsProvider;
 
     public void Insert(User user)
     {
-        _smsProvider.Send("");
+        _smsProvider.Send("Message");
         _dbContext.Users.Add(user);
         _dbContext.SaveChanges();
     }
-    public void Update(User user) { }
-
-    public void Delete(User user) { }
-
 }
